edd8d5b118da03a21d0b54f47174d88248fc1512
zwe-htet-paing <zwehtetpaing.works@gmail.com>
2024-12-06 05:50:42 +0700

    Fix typo

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from gradio_client import Client, handle_file
 
-# Read the image and encode it in Base64
-image_path = "./images/test_image.jpg"  # Replace with your image path
+image_path = "./images/test_image.jpg"
 client = Client("http://127.0.0.1:7860/")
 result = client.predict(
         image=handle_file(image_path),
